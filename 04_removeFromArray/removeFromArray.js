79ebee1423b70505ec2f53c99c6be80298b88751
Kei <barcelogreison@gmail.com>
2025-02-25 21:20:26 +0800

    finishing some js exercises ...

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...remove) {
+    return arr.filter(item => !remove.includes(item))
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+// console.log(removeFromArray([1, 2, 3, 4], 3));
